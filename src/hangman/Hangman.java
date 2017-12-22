261d993c4fbce6e0c60e768f2fc12466ae3d0e28
kevinrhu <kevinrouhu@gmail.com>
2017-12-21 21:09:46 -0500

    chose random word

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -4,8 +4,9 @@
  * and open the template in the editor.
  */
 package hangman;
-
-/**
+import java.lang.*;
+import java.util.*;
+/*
  *
  * @author Pineapplelord
  */
@@ -15,7 +16,24 @@ public class Hangman {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] words = {"cheese","ducks","bob"};
+        int index = (int)(Math.random() * 3);
+        String chosenWord = words[index];
+        
+        System.out.print("Your chosen word is: ");
+        System.out.println(chosenWord);
+        
+        System.out.print("Please print your Letter:");
+        char guess = 'a';
+        
+        char[] charArray = chosenWord.toCharArray();
+        int wordLength = charArray.length;
+        boolean[] array = new boolean[wordLenth];
+        
+        for (int i = 0; i < wordLength; i = i+1) {
+            boolean found = guess == charArray[i];
+        }
+        
     }
     
 }
