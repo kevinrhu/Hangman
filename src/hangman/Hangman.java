f6dbef58dd572b0f8d90720a611d3373df056c65
kevinrhu <kevinrouhu@gmail.com>
2017-12-30 20:46:09 -0500

    selects a word and you can add letters

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -4,8 +4,10 @@
  * and open the template in the editor.
  */
 package hangman;
+
 import java.lang.*;
 import java.util.*;
+
 /*
  *
  * @author Pineapplelord
@@ -16,24 +18,24 @@ public class Hangman {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] words = {"cheese","ducks","bob"};
-        int index = (int)(Math.random() * 3);
+        Scanner scanner = new Scanner(System.in);
+        boolean flag = false;
+        String[] words = {"cheese", "ducks", "bob"};
+        int index = (int) (Math.random() * 3);
         String chosenWord = words[index];
-        
+
         System.out.print("Your chosen word is: ");
         System.out.println(chosenWord);
-        
-        System.out.print("Please print your Letter:");
-        char guess = 'a';
-        
-        char[] charArray = chosenWord.toCharArray();
-        int wordLength = charArray.length;
-        boolean[] array = new boolean[wordLenth];
-        
-        for (int i = 0; i < wordLength; i = i+1) {
-            boolean found = guess == charArray[i];
+
+        while (flag == false) {
+            System.out.print("Please print your Letter:");
+            String letters = scanner.nextLine();
+            char c;
+            c = letters.charAt(0);
+            char[] charArray = chosenWord.toCharArray();
+            int wordLength = charArray.length;
+
         }
-        
     }
-    
+
 }
